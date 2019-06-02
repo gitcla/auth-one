1f53d1a6047c321346344e234971fd81e8e7230f
Claudio Coppari <claudiocup@gmail.com>
2019-06-02 14:41:56 +0200

    Added k8s config for whoami-service

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -4,9 +4,9 @@
 kubectl create -f mongo/service.yaml
 kubectl create -f mongo/deployment.yaml
 
-# Backend
-#kubectl create -f backend/service.yaml
-#kubectl create -f backend/deployment.yaml
+# whoami-service
+kubectl create -f whoami-service/k8s-service.yaml
+kubectl create -f whoami-service/k8s-deployment.yaml
 
 # Frontend
 #kubectl create -f frontend/service.yaml
